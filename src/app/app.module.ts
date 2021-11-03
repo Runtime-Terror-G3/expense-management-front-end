85ba1098c3dc009741d042951df9200fa101ff72
CristianRotar <cristianrotar04@gmail.com>
2021-11-03 20:20:28 +0200

    CSS added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
@@ -10,7 +9,7 @@ import { AddExpenseComponent } from './add-expense/add-expense.component';
 @NgModule({
   declarations: [	
     AppComponent,
-      AddExpenseComponent
+    AddExpenseComponent
    ],
   imports: [
     BrowserModule,
