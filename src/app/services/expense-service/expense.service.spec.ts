35172e4f2bead40ac47cd6f5fdc241124c73e63d
CristianRotar <cristianrotar04@gmail.com>
2021-11-03 18:05:55 +0200

    added provider in spec file

--- src/app/services/expense-service/expense.service.spec.ts
+++ src/app/services/expense-service/expense.service.spec.ts
@@ -1,11 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ExpenseService } from './expense.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('Service: ExpenseService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ExpenseService]
     });
   });
