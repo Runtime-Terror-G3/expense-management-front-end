ad2ecd22bd81ca522e6d9a007cc06b7ba8a92863
Anton Puț <anton.put@stud.ubbcluj.ro>
2021-11-08 21:03:10 +0200

    fixed build errors and fixed old styling issues

--- src/app/create-account/create-account.component.spec.ts
+++ src/app/create-account/create-account.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateAccountComponent } from './create-account.component';
@@ -8,7 +9,7 @@ describe('CreateAccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateAccountComponent ]
+      declarations: [ CreateAccountComponent ], imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
