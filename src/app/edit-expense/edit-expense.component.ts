80d84d3f933ea74316356f5fa89cffc53f2e5ea9
Adrian Pop <adi.pop.bv@gmail.com>
2021-11-25 14:56:37 +0200

    Added base ui for edit expense

--- src/app/edit-expense/edit-expense.component.ts
+++ src/app/edit-expense/edit-expense.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ExpenseCategory} from "../models/expense-category.enum";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ExpenseService} from "../services/expense-service/expense.service";
+import {SessionService} from "../services/session.service";
+import {IExpense} from "../models/expense.model";
 
 @Component({
   selector: 'app-edit-expense',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-expense.component.css']
 })
 export class EditExpenseComponent implements OnInit {
+  expenseCategory = ExpenseCategory;
+  categories: ExpenseCategory[] = [
+    ExpenseCategory.Clothing,
+    ExpenseCategory.Education,
+    ExpenseCategory.Entertainment,
+    ExpenseCategory.Food,
+    ExpenseCategory.Health,
+    ExpenseCategory.Housekeeping,
+    ExpenseCategory.SelfCare,
+    ExpenseCategory.Others,
+  ] as ExpenseCategory[];
+  form = new FormGroup({
+    category: new FormControl(''),
+    date: new FormControl('')
+  });
+  expenses: IExpense[] = [
+    {userId: 1, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 2, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+  ] as IExpense[];
 
-  constructor() { }
+  constructor(
+    private expenseService: ExpenseService,
+    private sessionService: SessionService,
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  get category() {
+    return this.form.get('category') as FormControl;
+  }
+
+  get date() {
+    return this.form.get('date') as FormControl;
+  }
+
+  filterExpenses() {
+
+  }
 }
