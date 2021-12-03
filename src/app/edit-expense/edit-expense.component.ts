ed7876e003fe0a375e1dd9f6b55de445fa579a30
Adrian Pop <adi.pop.bv@gmail.com>
2021-12-03 20:14:58 +0200

    Table now working as expected

--- src/app/edit-expense/edit-expense.component.ts
+++ src/app/edit-expense/edit-expense.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ExpenseCategory} from "../models/expense-category.enum";
-import {FormControl, FormGroup} from "@angular/forms";
 import {ExpenseService} from "../services/expense-service/expense.service";
-import {SessionService} from "../services/session.service";
 import {IExpense} from "../models/expense.model";
 
 @Component({
@@ -22,33 +20,64 @@ export class EditExpenseComponent implements OnInit {
     ExpenseCategory.SelfCare,
     ExpenseCategory.Others,
   ] as ExpenseCategory[];
-  form = new FormGroup({
-    category: new FormControl(''),
-    date: new FormControl('')
-  });
+  allExpenses: IExpense[] = [
+    {userId: 1, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 2, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 3, amount: 1, category: ExpenseCategory.Entertainment, date: new Date()} as IExpense,
+    {userId: 4, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 5, amount: 1, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 6, amount: 2, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 7, amount: 1, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 8, amount: 2, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 9, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 10, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 11, amount: 1, category: ExpenseCategory.Others, date: new Date()} as IExpense,
+    {userId: 12, amount: 2, category: ExpenseCategory.Housekeeping, date: new Date()} as IExpense,
+    {userId: 13, amount: 1, category: ExpenseCategory.SelfCare, date: new Date()} as IExpense,
+    {userId: 14, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 15, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 16, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+  ] as IExpense[];
   expenses: IExpense[] = [
     {userId: 1, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
     {userId: 2, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 3, amount: 1, category: ExpenseCategory.Entertainment, date: new Date()} as IExpense,
+    {userId: 4, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 5, amount: 1, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 6, amount: 2, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 7, amount: 1, category: ExpenseCategory.Food, date: new Date()} as IExpense,
+    {userId: 8, amount: 2, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 9, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 10, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 11, amount: 1, category: ExpenseCategory.Others, date: new Date()} as IExpense,
+    {userId: 12, amount: 2, category: ExpenseCategory.Housekeeping, date: new Date()} as IExpense,
+    {userId: 13, amount: 1, category: ExpenseCategory.SelfCare, date: new Date()} as IExpense,
+    {userId: 14, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
+    {userId: 15, amount: 1, category: ExpenseCategory.Clothing, date: new Date()} as IExpense,
+    {userId: 16, amount: 2, category: ExpenseCategory.Education, date: new Date()} as IExpense,
   ] as IExpense[];
+
+  category: string = '';
+  date: string = '';
 
   constructor(
     private expenseService: ExpenseService,
-    private sessionService: SessionService,
   ) {
   }
 
   ngOnInit(): void {
   }
 
-  get category() {
-    return this.form.get('category') as FormControl;
-  }
-
-  get date() {
-    return this.form.get('date') as FormControl;
-  }
-
   filterExpenses() {
-
+    this.expenses.length = 0;
+    for (let expense of this.allExpenses) {
+      if (this.expenseCategory[expense.category] == this.category
+        || this.category == 'All') {
+        if (new Date(this.date).toLocaleDateString('en-US') == expense.date.toLocaleDateString('en-US')
+          || this.date == '') {
+          this.expenses.push(expense);
+        }
+      }
+    }
   }
 }
